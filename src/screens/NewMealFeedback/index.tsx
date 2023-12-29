70b70825fbfea6b85e6513bcf3a5b1f5815e9a5d
Antonio Alves <antonio.jpsalves@gmail.com>
2023-12-29 20:26:04 -0300

    feat: feedback screen finished

--- src/screens/NewMealFeedback/index.tsx
+++ src/screens/NewMealFeedback/index.tsx
@@ -1,8 +1,62 @@
-import { Container } from "./styled"
+import { useNavigation, useRoute } from "@react-navigation/native"
+import { Container, Title, SpanText, BoldPhrase, Ilustration } from "./styled"
+
+import positiveIlustration from '../../assets/positive-ilustration.png'
+import negativeIlustration from '../../assets/negative-ilustration.png'
+import { DefaultBlackButton } from "../../components/DefaultBlackButton"
+import { useTheme } from "styled-components"
+
+type RouteParams = {
+  followingDiet: boolean
+}
+
 
 export function NewMealFeedBack() {
+
+  const theme = useTheme()
+  const route = useRoute()
+
+
+  const { followingDiet } = route.params as RouteParams
+
+  const informationContent = {
+    title: followingDiet ? "Continue assim!" : "Que pena!",
+    bold: followingDiet ? " dentro da dieta" : " saiu da dieta ",
+    startPhrase: followingDiet ? "Você continua" : "Você",
+    endPhrase: followingDiet ? ". Muito bem!" : "dessa vez, mas continue se esforçando e não desista!"
+  }
+
+  const navigation = useNavigation()
+
+  function handleGoBackHome() {
+    return navigation.navigate('home')
+  }
+
   return (
     <Container>
+      <Title style={{
+        color: followingDiet ? theme.COLORS.PRODUCT.GREEN_DARK : theme.COLORS.PRODUCT.RED_DARK
+      }}>
+        {informationContent.title}
+      </Title>
+      <SpanText>
+        {informationContent.startPhrase}
+        <BoldPhrase>{informationContent.bold}</BoldPhrase>
+        {informationContent.endPhrase}
+      </SpanText>
+
+      <Ilustration source={followingDiet ? positiveIlustration : negativeIlustration} />
+
+      <DefaultBlackButton
+        title="Ir para a página inicial"
+        style={{
+          height: 50,
+          width: 192,
+          paddingVertical: 16,
+          paddingHorizontal: 24
+        }}
+        onPress={handleGoBackHome}
+      />
 
     </Container>
   )
--- src/screens/NewMealFeedback/styled.ts
+++ src/screens/NewMealFeedback/styled.ts
@@ -6,4 +6,32 @@ export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_7};
   padding: 24px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  text-align: center;
+`
+
+export const BoldPhrase = styled.Text`
+   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_1};
+`
+
+export const SpanText = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_1};
+  text-align: center;
+`
+
+export const Ilustration = styled.Image`
+  margin-top: 50px;
+  width: 224px;
+  height: 288px;
+  flex-shrink: 0;
 `
