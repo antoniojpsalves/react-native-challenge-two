eba8ec84114b72c6ba2cdea2909118caf268b14a
Antonio Alves <antonio.jpsalves@gmail.com>
2024-01-03 11:56:03 -0300

    feat: layout finalizado

--- src/screens/Meal/index.tsx
+++ src/screens/Meal/index.tsx
@@ -1,7 +1,29 @@
-import { useTheme } from "styled-components"
-import { Container, Content } from "./styled"
-import { DefaultTitleHeader } from "../../components/DefaultTitleHeader"
-import { useRoute } from "@react-navigation/native"
+import { useTheme } from 'styled-components'
+
+import {
+  Container,
+  Content,
+  Span,
+  Title,
+  SubTitle,
+  Tag,
+  Symbol,
+  SpanOfTag,
+  ContainerButtons,
+  IconPencil,
+  LabelForButton,
+  LabelForButtonWhite,
+  IconTrash,
+  ButtonEdit,
+  ButtonTrash,
+  ContainerData
+} from './styled'
+
+import { DefaultTitleHeader } from '../../components/DefaultTitleHeader'
+
+import { useRoute } from '@react-navigation/native'
+
+import { TouchableOpacityProps } from 'react-native'
 
 type RouteParams = {
   mealId: string
@@ -19,7 +41,32 @@ export function Meal() {
     <Container style={{ backgroundColor: inDiet ? theme.COLORS.PRODUCT.GREEN_LIGHT : theme.COLORS.PRODUCT.RED_LIGHT }}>
       <DefaultTitleHeader title="Refeição" />
       <Content>
-
+        <ContainerData>
+          <Title>Sanduíche</Title>
+          <Span>Sanduíche de pão integral com atum e salada de alface e tomate.</Span>
+          <SubTitle>Data e Hora</SubTitle>
+          <Span>12/08/2022 às 16h00</Span>
+          <Tag>
+            <Symbol style={{
+              backgroundColor: inDiet ? theme.COLORS.PRODUCT.GREEN_DARK : theme.COLORS.PRODUCT.RED_DARK
+            }} />
+            <SpanOfTag>
+              {
+                inDiet ? 'dentro da dieta' : 'fora da dieta'
+              }
+            </SpanOfTag>
+          </Tag>
+        </ContainerData>
+        <ContainerButtons>
+          <ButtonEdit>
+            <IconPencil />
+            <LabelForButtonWhite>Editar refeição</LabelForButtonWhite>
+          </ButtonEdit>
+          <ButtonTrash>
+            <IconTrash />
+            <LabelForButton>Excluir refeição</LabelForButton>
+          </ButtonTrash>
+        </ContainerButtons>
       </Content>
     </Container>
   )
--- src/screens/Meal/styled.ts
+++ src/screens/Meal/styled.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components/native'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { TouchableOpacity } from 'react-native'
+import { PencilLine, Trash } from 'phosphor-react-native'
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
@@ -16,4 +18,103 @@ export const Content = styled.View`
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
   padding: 24px;
+  justify-content: space-between;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_1};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  margin-bottom: 16px;
+`
+
+export const Span = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_2};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  margin-bottom: 16px;
+`
+
+export const SubTitle = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_1};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  margin-bottom: 10px;
+`
+
+export const Tag = styled.View`
+  background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_6};
+  width: 144px;
+  height: 34px;
+  border-radius: 34px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  gap: 6px;
+  margin-top: 16px;
+`
+
+export const SpanOfTag = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_2};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+`
+
+export const Symbol = styled.View`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+`
+
+export const ButtonEdit = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 100%;
+  height: 50px;
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.COLORS.BASE.GRAY_2};
+`
+
+export const ButtonTrash = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 100%;
+  height: 50px;
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.COLORS.BASE.GRAY_2};
+`
+
+export const IconPencil = styled(PencilLine).attrs({
+  size: 18,
+})`
+  color: ${({ theme }) => theme.COLORS.BASE.WHITE};
+`
+
+export const IconTrash = styled(Trash).attrs({
+  size: 18
+})`
+  color: ${({ theme }) => theme.COLORS.BASE.GRAY_2};
+`
+
+export const LabelForButton = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`
+export const LabelForButtonWhite = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  color: ${({ theme }) => theme.COLORS.BASE.WHITE};
+`
+
+export const ContainerData = styled.View`
+  
+`
+
+export const ContainerButtons = styled.View`
+  gap: 10px;
+  margin-bottom: 10px;
 `
